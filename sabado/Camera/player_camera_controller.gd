8a20cb2fc2ef42066f8cc28c7d727bd57c57f62a
Alu <vitinh.do.cs@gmail.com>
2026-03-21 14:52:53 -0300

    nem lembro

--- sabado/Camera/player_camera_controller.gd
+++ sabado/Camera/player_camera_controller.gd
@@ -3,19 +3,53 @@ class_name PlayerCameraController
 
 @export var orbit_distance : float
 @export var orbit_height : float
+@export var orbit_speed : float
 
-var _direction : Vector2
-
+var _wall_distance : float
 var _rotation : float
+var _raycast : RayCast3D
 
 func set_camera(cam: SubCamera) -> void:
 	_camera = cam
-	_direction = Vector2(_camera.global_basis.z.x, _camera.global_basis.z.z) * orbit_distance
+	_rotation = global_rotation.y
+	_wall_distance = 0
+	
+	_raycast = RayCast3D.new()
+	add_child(_raycast)
+	pass
+
+func resign_camera() -> void:
+	super()
+	_raycast.queue_free()
 	pass
 
 func _update_pos(delta: float):
+	
+	var rotatedOffset = Vector3.BACK.rotated(Vector3.UP, _rotation) * (orbit_distance - _wall_distance)
+	
 	_camera.global_position = lerp(_camera.global_position, global_position +\
-			Vector3(_direction.x, orbit_height, _direction.y), easing)
+			Vector3(rotatedOffset.x, orbit_height, rotatedOffset.z), easing * delta)
 	
 	_camera.look_at(global_position)
+	pass
+
+func _process(delta: float) -> void:
+	# rot input
+	if(Input.is_action_pressed("in_rot_right")):
+		_rotation = lerp_angle(_rotation, _rotation + orbit_speed * delta, 1)
+		pass
+	if(Input.is_action_pressed("in_rot_left")):
+		_rotation = lerp_angle(_rotation, _rotation - orbit_speed * delta, 1)
+		pass
+
+func _physics_process(delta: float) -> void:
+	super(delta)
+	_raycast.target_position = Vector3.BACK.rotated(Vector3.UP, _rotation) * orbit_distance
+	_raycast.target_position.y = orbit_height
+	if _raycast.is_colliding():
+		_wall_distance = (_raycast.global_position + _raycast.target_position -\
+				_raycast.get_collision_point()).length()
+		
+	else:
+		_wall_distance = 0
 	pass
--- sabado/Player/player_character.gd
+++ sabado/Player/player_character.gd
@@ -8,21 +8,37 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 const ROT_SPEED = 15
 
+var input_dir : Vector3
+
+func _input(event: InputEvent) -> void:
+	
+	var input = Input.get_vector("in_left", "in_right", "in_up", "in_down")
+	
+	input_dir = camera.basis * Vector3(input.x, 0, input.y)
+	input_dir.y = 0
+	input_dir = input_dir.normalized()
+	pass
+
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
-		velocity += get_gravity() * delta
-
-
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var input_dir := Input.get_vector("in_left", "in_right", "in_up", "in_down")
-	var direction := (camera.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+		velocity += get_gravity()*2 * delta
+	
+	var move_vec = input_dir * SPEED
+	
+	if get_floor_angle(Vector3.UP) > 0.4:
+		floor_stop_on_slope = false
+		move_vec += get_floor_normal() * SPEED
+	else:
+		floor_stop_on_slope = true
+		pass
+	
+	if move_vec:
+		velocity.x = move_vec.x 
+		velocity.z = move_vec.z
 		
-		var target_angle = Vector3.BACK.signed_angle_to(direction, Vector3.UP)
+		# Rotate
+		var target_angle = Vector3.BACK.signed_angle_to(move_vec, Vector3.UP)
 		var current_angle = Vector3.BACK.signed_angle_to(mesh.global_basis.z, Vector3.UP)
 		
 		target_angle = lerp_angle(current_angle, target_angle, 1)
@@ -31,11 +47,14 @@ func _physics_process(delta: float) -> void:
 			mesh.global_rotation.y += sign(target_angle - current_angle) * ROT_SPEED * delta
 		else:
 			mesh.global_rotation.y += target_angle - current_angle
-		
+			pass
 		
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		pass
+	
+	
+	
 	move_and_slide()
 	
